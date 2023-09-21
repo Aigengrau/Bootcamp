03e2304489dc19b6b1c50aef08fb32c563f72246
Aigengrau <ochilovanis@gmail.com>
2023-09-21 15:31:26 +0300

    first commit

---
+++ bootcamp/src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+          <a href="/"className="navbar-brand">Frontend Bootcamp</a>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default App;
